e9c6446a30684e77774e85016c6e719416e77e01
Shakhboz <Shfazliddinov1940@gmail.com>
2025-03-19 19:59:38 +0500

    contacts info is ready

---
+++ routers/contacts_info.py
@@ -0,0 +1,8 @@
+from aiogram import Router, F
+from aiogram.types import Message
+router = Router()
+
+@router.message(F.text.lower() == "полезные контакты")
+async def contacts_info_handler(message: Message):
+    print(f"Received text: {message.text}")
+    await message.answer("Hello! Here is a contacts")
